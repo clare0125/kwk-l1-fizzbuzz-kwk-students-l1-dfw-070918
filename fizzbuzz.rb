f80eb37ff78880c43288344fd4d33596547f9f89
clare0125 <clare.woozie1@gmail.com>
2018-07-10 21:47:43 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "Fizzbuzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  end
+end
+        
